f0f04b75615065448d78a40530f2acbab20790d9
DeepsYo2 <deepscse2002@gmail.com>
2024-12-01 00:00:57 +0530

    Update APB_Controller_Interface.v

--- APB_Controller_Interface.v
+++ APB_Controller_Interface.v
@@ -1,103 +1,36 @@
 /*
-// TITLE :          APB CONTROLLER_tb.v
+// TITLE :          APB Controller INTERFACE.v
 
 // Created by :     Mr. Deepak Kumar
 
-// Duration :       18 Nov 2024 to 21 Nov 2024
+// Duration :       24 Nov 2024 to 25 Nov 2024
 
 // Date of publish : 30 Nov 2024
 */
 
 
+module APB_Controller_Interface( 
+    input Pwrite, Penable, 
+    input[2:0] Pselx, 
+    input[31:0] Paddr, Pwdata, 
+    output Pwrite_out, Penable_out, 
+    output[2:0] Pselx_out, 
+    output[31:0] Paddr_out, Pwdata_out, 
+    output reg[31:0] Prdata 
+);
 
-module APB_Controller_tb();
+assign Pwrite_out = Pwrite;
+assign Pselx_out = Pselx;
+assign Paddr_out = Paddr;
+assign Pwdata_out = Pwdata;
+assign Penable_out = Penable;
 
-
-// Declare the ports
-reg Hclk;
-reg Hresetn;
-reg Hwrite;
-reg valid;
-reg Hwritereg;
-reg [31:0] Haddr;
-reg [31:0] Hwdata;
-reg [31:0] Haddr1;
-reg [31:0] Haddr2;
-reg [31:0] Hwdata1;
-reg [31:0] Hwdata2;
-reg [2:0] tempselx;
-
-wire Pwrite;
-wire Penable;
-wire Hreadyout;
-wire [2:0] Pselx;
-wire [31:0] Pwdata;
-wire [31:0] Paddr;
-wire [31:0] Prdata;
-
-
-// Instantiate the APB Controller
-APB_Controller DUT( .Hclk(Hclk), .Hresetn(Hresetn), .Hwrite(Hwrite), .valid(valid),
-		    .Hwritereg(Hwritereg), .Haddr(Haddr), .Hwdata(Hwdata), .Haddr1(Haddr1),
-		    .Haddr2(Haddr2), .Hwdata1(Hwdata1), .Hwdata2(Hwdata2), .tempselx(tempselx),
-		    .Pwrite(Pwrite), .Penable(Penable), .Hreadyout(Hreadyout), .Pselx(Pselx),
-		    .Pwdata(Pwdata), .Paddr(Paddr), .Prdata(Prdata) );
-
-
-// Clock Generation
-initial
+always @(*)
   begin
-	Hclk = 1'b0;
-	forever #10
-	Hclk = ~Hclk;
-  end
-
-
-// Reset task
-task r();
-  begin
-	@(negedge Hclk)
-		Hresetn = 1'b0;
-	#10;
-		Hresetn = 1'b1;
-  end
-endtask
-
-
-// Input Stimulus task
-task in(input a, input [31:0] b, input [31:0] c, input [31:0] d, input [31:0] e, input [31:0] f, input [31:0] g, input [2:0] h, input w);
-  begin
-	valid = a;
-    	Haddr = b;
-    	Hwdata = c;
-    	Haddr1 = d;
-    	Haddr2 = e;
-    	Hwdata1 = f;
-    	Hwdata2 = g;
-    	tempselx = h;
-    	Hwritereg = w;
-  end
-endtask
-
-// Stimulus Generation
-initial
-  begin
-	r;
-	#10;
-	
-	// Apply first set of inputs (Write operation)
-    	in(1'b1, 32'h8c00_1234, 32'h8500_0000, 32'h8040_0000, 32'h8000_0000, 32'h0000_0000, 32'h0000_0000, 3'b001, 1'b1);
-    	#10;
-    
-    	// Apply second set of inputs (Read operation)
-    	in(1'b1, 32'h8040_0000, 32'h8000_0000, 32'h8040_0000, 32'h8000_0000, 32'h0000_0000, 32'h0000_0000, 3'b010, 1'b0);
-    	#10;
-    
-    	// Apply third set of inputs (Write operation)
-    	in(1'b1, 32'h8c00_5678, 32'h9500_0000, 32'h8040_1111, 32'h9000_1111, 32'h0000_0000, 32'h0000_0000, 3'b011, 1'b1);
-    	#10;
-
-	$finish;    // End Simulatioin
+    	if (!Pwrite && Penable)
+	  begin
+        	Prdata = 32'd25;
+  	  end
   end
 
 endmodule
